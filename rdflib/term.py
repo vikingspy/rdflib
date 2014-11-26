3507f7d4564bd515635312333634ede5c8ebd933
Joern Hees <dev@joernhees.de>
2014-11-27 00:01:08 +0100

    six: use PY2, PY3 from six, __nonzero__/__bool__ compatibility

--- rdflib/term.py
+++ rdflib/term.py
@@ -59,6 +59,8 @@ from hashlib import md5
 import rdflib
 from . import py3compat
 from .compat import numeric_greater
+from .py3compat import PY2
+from .py3compat import PY3
 from .py3compat import b
 from .py3compat import urldefrag
 from .py3compat import urljoin
@@ -246,7 +248,7 @@ class URIRef(Identifier):
     def __getnewargs__(self):
         return (unicode(self), )
 
-    if not py3compat.PY3:
+    if PY2:
         def __str__(self):
             return self.encode()
 
@@ -402,7 +404,7 @@ class BNode(Identifier):
     def __reduce__(self):
         return (BNode, (unicode(self),))
 
-    if not py3compat.PY3:
+    if PY2:
         def __str__(self):
             return self.encode()
 
@@ -659,7 +661,7 @@ class Literal(Identifier):
         s = unicode.__add__(self, val)
         return Literal(s, self.language, self.datatype)
 
-    def __nonzero__(self):
+    def __bool__(self):
         """
         Is the Literal "True"
         This is used for if statements, bool(literal), etc.
@@ -667,6 +669,9 @@ class Literal(Identifier):
         if self.value != None:
             return bool(self.value)
         return len(self) != 0
+
+    if PY2:
+        __nonzero__ = __bool__
 
     @py3compat.format_doctest_out
     def __neg__(self):
@@ -1256,7 +1261,7 @@ class Literal(Identifier):
                         '"', '\\"').replace(
                             '\r', '\\r')
 
-    if not py3compat.PY3:
+    if PY2:
         def __str__(self):
             return self.encode()
 
@@ -1298,7 +1303,7 @@ class Literal(Identifier):
 
 
 def _parseXML(xmlstring):
-    if not py3compat.PY3:
+    if PY2:
         xmlstring = xmlstring.encode('utf-8')
     retval = xml.dom.minidom.parseString(
         "<rdflibtoplevelelement>%s</rdflibtoplevelelement>" % xmlstring)
